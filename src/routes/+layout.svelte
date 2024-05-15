b300049c18a26296b1688bdee472e60f08aba20a
AidanDonnelly1 <aido4381@colorado.edu>
2024-05-15 12:10:31 -0600

    added bg gradient; made tailwind work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-
+    import "../app.css";
 </script>
 
-<div class="">
+<div class="min-h-screen bg-gradient-to-r from-fuchsia-900 from-10% via-pink-900 to-rose-900">
     <slot />
 </div>
