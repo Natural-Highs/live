7c4ee3c1d244324e1d1e7d173b301312213064de
AidanDonnelly1 <aido4381@colorado.edu>
2024-11-03 20:33:15 +0000

    switched to use daisyUI

--- src/routes/admin/survey/+page.svelte
+++ src/routes/admin/survey/+page.svelte
@@ -1,72 +1,48 @@
 <script lang="ts">
-    export let data;
-    import { goto } from "$app/navigation"; //import for reroute
+  export let data;
+  import { goto } from "$app/navigation"; //import for reroute
 
-    function addsurvey(surveyID: string) { //rerouting
-        goto(`/admin/survey/templates/${surveyID}`);
-    }
+  function addsurvey(surveyID: string) {
+    //rerouting
+    goto(`/admin/survey/templates/${surveyID}`);
+  }
 
-    import { page } from "$app/stores";
-    let users = $page.data.users; //grab our users from the db from page.server.ts
+  import { page } from "$app/stores";
+  let users = $page.data.users; //grab our users from the db from page.server.ts
 
-    //console.log("script", users); (ucomment to see final array before it's inserted)
-
+  //console.log("script", users); (ucomment to see final array before it's inserted)
 </script>
 
-<div class="table-container">
-    <table class="table">
-        <thead style="font-size:large">
-            <tr>
-                <th>UUID</th>
-                <th>Name</th>
-                <th>Edit</th>
-                <th>Administer</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each data?.surveys as survey}
-                <tr>
-                    <td>{survey.id}</td>
-                    <td>{survey.name}</td>
-                    <td><button> <i class="fa-solid fa-pencil" /></button></td>
-                    <td
-                        ><button>
-                            <i class="fa-solid fa-user-tie" />
-                        </button></td
-                    >
-                </tr>
-            {/each}
-        </tbody>
-    </table>
+<div class="overflow-x-auto px-8">
+  <table class="table">
+    <thead class="test-lg">
+      <tr>
+        <th>UUID</th>
+        <th>Name</th>
+        <th>Edit</th>
+        <th>Administer</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each data?.surveys as survey}
+        <tr>
+          <td>{survey.id}</td>
+          <td>{survey.name}</td>
+          <td><button> <i class="fa-solid fa-pencil" /></button></td>
+          <td
+            ><button>
+              <i class="fa-solid fa-user-tie" />
+            </button></td
+          >
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 </div>
 
 <!-- not sure how we're adding in the survey id rout, but for now button takes you to a page with whatever parameter was passed in -->
-<button style="padding-left: 40px" class="add-button" on:click={() => addsurvey("whateverthisidis")} ><i class="fa-solid fa-plus" /> Add Survey</button>
-
-<style>
-    .table-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        flex: 1;
-        padding-top: 24px;
-        padding-left: 24px;
-        padding-right: 24px;
-        padding-bottom: 12px;
-    }
-
-    .table {
-        padding-left: 90%;
-        border: 2px solid #333;
-        height: 100%;
-        width: 100%;
-        font-size: medium;
-    }
-
-    .table th,
-    .table td {
-        padding: 10px;
-        width: 25%;
-    }
-</style>
+<button
+  class="add-button pl-12"
+  on:click={() => addsurvey("whateverthisidis")}
+  ><i class="fa-solid fa-plus" /> Add Survey</button
+>
