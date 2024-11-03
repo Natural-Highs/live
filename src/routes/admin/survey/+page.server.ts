9ea6689831c52569fd0220bd59eb3295126942b8
AidanDonnelly1 <aido4381@colorado.edu>
2024-11-03 20:27:39 +0000

    fixed docID; made it so it is surveys instead of users

--- src/routes/admin/survey/+page.server.ts
+++ src/routes/admin/survey/+page.server.ts
@@ -2,23 +2,23 @@
 import { db } from "../../../lib/firebase/firebase"; 
 import type { PageServerLoad } from "./$types";
 
-async function getUsers(){
-    let users = [];
-        const all_docs = await db.collection("users").get(); //get documents from users collection (doc contains fields for a user)
+async function getSurveys(){
+    let surveys = [];
+        const all_docs = await db.collection("surveys").get(); //get documents from users collection (doc contains fields for a user)
                                                             //await so this finishes fetching the documents (promise) before moving on
         if (!all_docs.empty) {
-            users = all_docs.docs.map(doc => ({ //.doc gives array of the document objects for each user, map for new array
-                id: doc.data().id, //.data() to get data from document
-                username: doc.data().username
+            surveys = all_docs.docs.map(doc => ({ //.doc gives array of the document objects for each user, map for new array
+                id: doc.id, //.data() to get data from document
+                name: doc.data().name
             }));
-            return users;
+            return surveys;
         } else {
             return []; //empty array if no documents found
         }
 }
 
 export const load: PageServerLoad = async () => {
-    const users = (await getUsers()) ?? []; //execute getusers, await because we need this data before moving on
+    const surveys = (await getSurveys()) ?? []; //execute getusers, await because we need this data before moving on
                                             //nullish coalescing operator (??) will return the right hand side if the left is null
-    return {users}; //return users object with document data, that will be available in a prop named users in +page.svelte
+    return {surveys}; //return users object with document data, that will be available in a prop named users in +page.svelte
 };
