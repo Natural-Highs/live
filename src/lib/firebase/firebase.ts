8a428e631f5ebbd21be9dc2d870770c76cccdb22
asavy79 <asavard@uoguelph.ca>
2024-11-06 17:44:35 -0700

    fixed quick firebase issue

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,18 +1,18 @@
-// // Import the functions you need from the SDKs you need
-// import type { ServiceAccount } from "firebase-admin";
-// import admin from "firebase-admin";
-// import serviceAccount from "../../../serviceAccount.json";
-// import { getAuth, connectAuthEmulator } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import type { ServiceAccount } from "firebase-admin";
+import admin from "firebase-admin";
+import serviceAccount from "../../../serviceAccount.json";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
-// // // Your web app's Firebase configuration
-// // const firebaseConfig = {
-// //   apiKey: import.meta.env.VITE_APIKEY,
-// //   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-// //   projectId: import.meta.env.VITE_PROJECT_ID,
-// //   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-// //   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-// //   appId: import.meta.env.VITE_APP_ID,
-// // };
+// // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
 
 if(!admin.apps.length) {
   admin.initializeApp({
