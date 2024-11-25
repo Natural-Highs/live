887ba8bc69b196fd9c139fa8ca67d1d14d7c71d3
asavy79 <asavard@uoguelph.ca>
2024-11-25 10:21:07 -0800

    added surveyName field in surveyresponses

--- src/routes/authentication/+page.svelte
+++ src/routes/authentication/+page.svelte
@@ -1,12 +1,58 @@
 <script>
   import { enhance } from "$app/forms";
-  export let form;
+  import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+  } from "firebase/auth";
+  import { auth } from "$lib/firebase/firebase.app";
+  import { authStore } from "../../store/store";
+  import Layout from "../+layout.svelte";
+  import { goto } from "$app/navigation";
 
   let register = false;
   let authenticating = false;
+  let email = "";
+  let password = "";
+  let errorMessage = "";
+  let password2 = "";
 
   function handleRegister() {
     register = !register;
+  }
+
+  if ($authStore.loading && $authStore.user != null) {
+    goto("/dashboard");
+  }
+
+  async function handleSubmit(event) {
+    errorMessage = "";
+    event.preventDefault();
+    if (register) {
+      console.log("Closing");
+      return;
+    }
+    authenticating = true;
+    authStore.update((store) => {
+      return {
+        ...store,
+        loading: true,
+      };
+    });
+
+    if (!register) {
+      try {
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        authenticating = false;
+        console.log($authStore);
+        return;
+      } catch (error) {
+        console.log(error);
+        errorMessage = "Invalid credentials!";
+      }
+    }
+    authenticating = false;
   }
 </script>
 
@@ -14,61 +60,118 @@
   <div
     class=" w-1/2 flex flex-col justify-self-center justify-items-center gap-4"
   >
-    <form
-      class="flex flex-col justify-start w-full gap-2"
-      method="POST"
-      action="?/handleAuth"
-      use:enhance
-    >
-      <h1 class="text-white text-center text-2xl font-semibold py-2">
-        {register ? "Register" : "Login"}
-      </h1>
-      {#if form?.message}
+    {#if register}
+      <form
+        class="flex flex-col justify-start w-full gap-2"
+        action="?/createUser"
+        method="POST"
+      >
+        <h1 class="text-white text-center text-2xl font-semibold py-2">
+          {register ? "Register" : "Login"}
+        </h1>
+        {#if errorMessage}
+          <div class="flex flex-row justify-center">
+            <p class="text-md font-semibold text-error">{errorMessage}</p>
+          </div>
+        {/if}
         <div class="flex flex-row justify-center">
-          <p class="text-md font-semibold text-error">{form?.message}</p>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            class="w-1/2 mx-2 p-2 rounded-md"
+            bind:value={email}
+          />
         </div>
-      {/if}
-      <div class="flex flex-row justify-center">
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          class="w-1/2 mx-2 p-2 rounded-md"
-        />
-      </div>
-      <div class="flex flex-row justify-center">
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          class="w-1/2 mx-2 p-2 rounded-md"
-        />
-      </div>
-      {#if register}
+        <div class="flex flex-row justify-center">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            class="w-1/2 mx-2 p-2 rounded-md"
+            bind:value={password}
+          />
+        </div>
         <div class="flex flex-row justify-center">
           <input
             type="password"
             name="confirmPass"
             placeholder="Confirm Password"
             class="w-1/2 mx-2 p-2 rounded-md"
+            bind:value={password2}
           />
         </div>
-      {/if}
 
-      <input type="hidden" id="regiester" name="register" value={register} />
+        <input type="hidden" id="regiester" name="register" value={register} />
 
-      <div class="flex flex-row justify-center">
-        <button type="submit" class="w-1/2 btn btn-primary">
-          {#if authenticating}
-            <p>Submitting</p>
-          {:else}
-            <p>Submit</p>
-          {/if}
-        </button>
-      </div>
-    </form>
+        <div class="flex flex-row justify-center">
+          <button
+            disabled={authenticating}
+            type="submit"
+            class="w-1/2 btn btn-primary"
+          >
+            {#if authenticating}
+              <p>Submitting</p>
+            {:else}
+              <p>Submit</p>
+            {/if}
+          </button>
+        </div>
+      </form>
+    {/if}
+    {#if !register}
+      <form
+        class="flex flex-col justify-start w-full gap-2"
+        on:submit={handleSubmit}
+      >
+        <h1 class="text-white text-center text-2xl font-semibold py-2">
+          Login
+        </h1>
+        {#if errorMessage}
+          <div class="flex flex-row justify-center">
+            <p class="text-md font-semibold text-error">{errorMessage}</p>
+          </div>
+        {/if}
+        <div class="flex flex-row justify-center">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            class="w-1/2 mx-2 p-2 rounded-md"
+            bind:value={email}
+          />
+        </div>
+        <div class="flex flex-row justify-center">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            class="w-1/2 mx-2 p-2 rounded-md"
+            bind:value={password}
+          />
+        </div>
+
+        <input type="hidden" id="regiester" name="register" value={register} />
+
+        <div class="flex flex-row justify-center">
+          <button
+            disabled={authenticating}
+            type="submit"
+            class="w-1/2 btn btn-primary"
+          >
+            {#if authenticating}
+              <p>Submitting</p>
+            {:else}
+              <p>Submit</p>
+            {/if}
+          </button>
+        </div>
+      </form>
+    {/if}
 
     <div class="flex flex-row justify-center">
       {#if register}
@@ -101,3 +204,9 @@
     </div>
   </div>
 </div>
+
+<button
+  on:click={() => {
+    console.log($authStore);
+  }}>Test</button
+>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,34 +1,106 @@
-<!-- <script>
-    import { getDoc, doc } from "firebase/firestore";
-    import { onMount } from 'svelte';
-    import { auth, db } from "$lib/firebase/firebase.app";
-    import { onAuthStateChanged } from "firebase/auth";
+<script>
+  import { auth } from "$lib/firebase/firebase.app";
+  import { authStore } from "../../store/store";
+  import { goto } from "$app/navigation";
+  import { signOut } from "firebase/auth";
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+  import { getAuth } from "firebase/auth";
+  import { enhance } from "$app/forms";
 
-    async function checkClient(user){
-        if (!user) {
-            window.location.href = "/authentication";
+  $: if (browser && !$authStore.loading && !$authStore.user) {
+    goto("/authentication");
+  }
+
+  let surveys = [];
+  let message = "";
+
+  onMount(async () => {
+    const unsubscribe = authStore.subscribe(async ({ user }) => {
+      if (user) {
+        // const clientAuth = getAuth();
+        // const user = clientAuth.currentUser;
+        // const idToken = await user?.getIdToken();
+        // console.log(idToken);
+        try {
+          const results = await fetch(`/api/surveys/?user=${user}`, {
+            method: "GET",
+          });
+          if (!results.ok) {
             return;
+          }
+
+          const data = await results.json();
+          console.log(data);
+          surveys = data;
+        } catch (error) {
+          console.log(error);
         }
-
-        console.log("Current user:", user.uid);
-        
-        // Retrieve user document from Firestore
-        const userDocRef = doc(db, "Users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-
-        if (!userDoc.exists()) {
-            window.location.href = "/authentication";
-        }
-    }
-
-    onMount(() => {
-        // Use onAuthStateChanged to reliably get the auth state
-        onAuthStateChanged(auth, (user) => {
-            checkClient(user);
-        });
+      }
     });
-</script> -->
+
+    return () => unsubscribe();
+  });
+
+  const submitAddSurvey = ({ form, data, action, cancel }) => {
+    message = "";
+    return async ({ result, formElement, update }) => {
+      console.log(result);
+      if (result.data.status === "success") {
+        surveys = [...surveys, result.data.data];
+      } else {
+        message = result.data.message;
+      }
+      formElement.reset();
+    };
+  };
+</script>
 
 <div>
-  <h1>Client Page</h1>
+  <div class="flex flex-col items-center space-y-3 mt-5">
+    <h1>Hello {$authStore.user ? $authStore.user : ""}</h1>
+    <button
+      on:click={async () => {
+        await signOut(auth);
+      }}
+      class="btn btn-primary">Sign Out</button
+    >
+  </div>
+
+  <h1 class="text-center mt-20 text-blue-500 font-extrabold text-lg">
+    Surveys
+  </h1>
+  <div class="flex flex-col items-center mt-3 space-y-3">
+    {#if surveys.length == 0}
+      <h1 class="text-gray-500">No Surveys to display!</h1>
+    {/if}
+    {#each surveys as survey}
+      <div class="card bg-base-100 w-96 shadow-xl">
+        <div class="card-body flex flex-col">
+          <p>Name: {survey?.surveyName}</p>
+          <p>ID: {survey?.surveyId}</p>
+        </div>
+      </div>
+    {/each}
+  </div>
+  <div class="card flex flex-col items-center mt-10">
+    <h1 class="card-title">Add Survey</h1>
+    <form
+      class="card-body form-control flex items-center space-y-3"
+      method="POST"
+      action="?/addSurvey"
+      use:enhance={submitAddSurvey}
+    >
+      <input
+        class="input input-bordered"
+        type="text"
+        name="surveyId"
+        required
+        placeholder="Survey Id..."
+      />
+      <input hidden name="userEmail" value={$authStore.user} />
+      <button class="btn btn-primary">Submit</button>
+      <p class="text-red-500">{message}</p>
+    </form>
+  </div>
 </div>
