a6ffc7a4579a28797a02de2fb322dae99b75e4a6
wistb <git@wistb.dev>
2025-11-01 04:30:07 -0600

    refactor: improve entry point type safety

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './app.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Failed to find root element');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
