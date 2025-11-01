add11f07eccc35774220b4c90ce2e6a1bfc28d5e
wistb <git@wistb.dev>
2025-11-01 04:28:49 -0600

    refactor: remove SvelteKit routing (migrated to React Router)

--- src/routes/api/logout/+server.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from '@sveltejs/kit';
-
-export const POST: RequestHandler = async ({cookies}) => {
-    cookies.delete("session",
-    {path: "/"});
-    console.log("cookie deleted");
-    return new Response(JSON.stringify({success: true}), {status: 200});
-}
