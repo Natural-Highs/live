98bb8d1c1f64b69eeb62c75cd92dc943ac347b13
wistb <git@wistb.dev>
2025-11-02 21:52:58 -0700

    fix: firebase env vars and leftover svelte file

--- src/lib/firebase/firebase.app.ts
+++ src/lib/firebase/firebase.app.ts
@@ -6,12 +6,12 @@ console.log('Script is running');
 
 // Firebase configuration (client-side)
 const firebaseConfig = {
-  apiKey: process.env.VITE_APIKEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 let firebaseApp: ReturnType<typeof initializeApp> | undefined;
@@ -28,8 +28,8 @@ if (!getApps().length) {
 export const db = getFirestore(firebaseApp);
 export const auth = getAuth(firebaseApp);
 
-console.log('Environment Mode:', process.env.MODE);
-if (process.env.MODE === 'development') {
+console.log('Environment Mode:', import.meta.env.MODE);
+if (import.meta.env.MODE === 'development') {
   connectFirestoreEmulator(db, 'localhost', 8080); // Use correct Firestore emulator port if different
   connectAuthEmulator(auth, 'http://localhost:9099'); // Use correct Auth emulator port if different
   console.log('Connected to Firestore and Auth emulators');
