b7e71a28b76469c348c7892baa74af52077d09d8
wistb <git@wistb.dev>
2025-11-05 22:03:43 -0700

    chore: add template react components

---
+++ src/components/ui/secondary-button.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+export interface SecondaryButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+  fullWidth?: boolean;
+}
+
+const SecondaryButton = React.forwardRef<HTMLButtonElement, SecondaryButtonProps>(
+  ({ children, className, fullWidth = true, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type="button"
+        className={`btn btn-secondary rounded-[20px] text-[20px] leading-[30px] font-normal ${
+          fullWidth ? 'w-full max-w-[338px]' : ''
+        } h-12 ${className || ''}`}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+SecondaryButton.displayName = 'SecondaryButton';
+
+export { SecondaryButton };
+
