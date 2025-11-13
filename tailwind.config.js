2d76c5ef9d7287a3ea17698346dc865648f0b3ed
Micah Gagerman <micahgagerman21@gmail.com>
2025-11-12 18:07:57 -0700

    fix font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"], // adjust paths to your project
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+// Tailwind v4 Configuration
+// Most configuration is now done via @theme directive in app.css
+// This file is optional in v4, but can be used for content paths and plugins
+
+export default {
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	// Colors and fonts are now defined in src/app.css using @theme
+	// See: https://tailwindcss.com/docs/v4-beta#configuring-with-css
 }
-
-module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                btnGreen: "#347937",
-                btnGrey: "#637B7C",
-                midGreen: "#98DA87",
-                bgGreen: "#E2FDE6",
-            },
-            fontFamily: {
-                inria: ['Inria Serif', 'sans-serif'], // text
-                inter: ['Inter', 'sans-serif'], // just because
-                kapakana: ['Kapakana', 'sans-serif'], // logo
-                kotta: ['Kotta One', 'sans-serif'], // buttons
-                lisu: ['Lisu Bosa', 'sans-serif'], // h1
-            }
-        },
-    },
-};
-
-/*
-// I found this online. Supposedly it'll set a default for all h1 elements
-// I just don't know where to put it in the file structure.
-    <style>
-      html * {
-        font-size: 37px;
-        font-family: Lisu Bosa, sans-serif;
-      }
-    </style>
-*/
