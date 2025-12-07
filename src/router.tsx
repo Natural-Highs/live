996405e64356affb2ede2f4c9af613e4e7ab874e
wistb <git@wistb.dev>
2025-12-07 09:22:24 -0700

    Issue #12: Provide queryClient in router context initialization

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,9 @@
+import {QueryClient} from '@tanstack/react-query'
 import {createRouter} from '@tanstack/react-router'
 import type {RouterContext} from './routes/__root'
 import {routeTree} from './routeTree.gen'
+
+const queryClient = new QueryClient()
 
 export function getRouter() {
 	const router = createRouter({
@@ -12,7 +15,8 @@ export function getRouter() {
 				loading: true,
 				consentForm: false,
 				admin: false
-			}
+			},
+			queryClient
 		} satisfies RouterContext
 	})
 
